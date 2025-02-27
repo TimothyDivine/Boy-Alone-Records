ebaff1e9101544e6850965e7b7809b2f6ae70d35
DIVINE <divinetimothyeiq454@gmail.com>
2025-02-28 00:38:57 +0100

    adding a login in form

---
+++ main.js
@@ -0,0 +1,13 @@
+let popup = document.getElementById("popup");
+let body = document.getElementById("login")
+
+function openPopup(){
+
+    popup.classList.add("open-popup");
+    login.classList.add("login");
+}
+
+function closePopup(){
+
+    popup.classList.remove("open-popup")
+}
