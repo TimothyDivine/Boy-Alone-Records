edb46c31ebd8f4cad4f4cd7472e9a76ce9c7f759
DIVINE <divinetimothyeiq454@gmail.com>
2025-03-06 15:11:29 +0100

    adding swith button and profile to the page

--- main.js
+++ main.js
@@ -11,3 +11,29 @@ function closePopup(){
 
     popup.classList.remove("open-popup");
 }
+
+let myResult = document.getElementById("result");
+let myBtn = document.getElementById("myBtn");
+
+function setUserName(){
+  const userName = document.getElementById("myUserName").value;
+  localStorage.setItem("name", userName);
+  myResult.textContent = userName;
+
+}
+
+if (!localStorage.getItem("name")){
+  setUserName();
+}
+
+else {
+  const storedName = localStorage.getItem("name");
+  myResult.textContent = storedName;
+}
+
+myBtn = document.getElementById("myBtn").onclick = () => {
+  setUserName();
+}
+
+
+
